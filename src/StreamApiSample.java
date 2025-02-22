b0ec06d52f0a5c46f5de29850e62edbe43bfa608
Saravanan <saran_jad@yahoo.co.in>
2025-02-22 02:53:29 +0000

    update

--- src/StreamApiSample.java
+++ src/StreamApiSample.java
@@ -25,6 +25,9 @@ public class StreamApiSample {
 		Mapper mapper = new Mapper(listOfEmployee);
 		mapper.map().stream().forEach(emp -> System.out.println(emp));
 		
+		List<String> list = Arrays.asList( "8" , "7", "36", "2" );
+	    List<Integer> intList = list.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+	    intList.stream().forEach(obj -> System.out.println(obj));    
 	}
 
 }
